a7d15c3a1c5792ef62f7d3cc14a1933fdbc083f7
grgutierrez <gr.gutierrez@ebc.edu.mx>
2025-12-03 12:23:49 -0600

    Add files via upload

---
+++ s23_createelement_js/README.md
@@ -0,0 +1,6 @@
+# S23_CreateElement_JS
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/GALA-REBECA-GUTIERREZRODRIGUEZ/pen/xbVJWjo](https://codepen.io/GALA-REBECA-GUTIERREZRODRIGUEZ/pen/xbVJWjo).
+
---
+++ s23_createelement_js/dist/script.js
@@ -0,0 +1,40 @@
+// referencia para los elementos //
+const imageUrlInput =
+document.getElementById('imageUrlInput');
+
+const addImageBtn =
+document.getElementById('addImageBtn');
+
+const gallery =
+document.getElementById('galleryContainer');
+
+// Evento, al hacer click en el boton //
+
+addImageBtn.addEventListener('click', () => {
+  const imageUrl = imageUrlInput.value.trim();
+  
+  if (imageUrl){
+    //crear un nuevo contenedors para la imagen//
+    const col = document.createElement('div');
+    col.classList.add('col');
+    
+    //crear la imagen//
+    
+    const img = document.createElement('img');
+    img.src = imageUrl;
+    img.classList.add('img-fluid', 'rounded', 'shadow-sm');
+    img.alt = 'Imagen añadida';
+    
+    // Agregar la imagen dentro del contenedor
+    col.appendChild(img);
+
+    // Añadir el contenedor a la galería
+    galleryContainer.appendChild(col);
+
+    // Limpiar input
+    imageUrlInput.value = '';
+  } else {
+    alert('Por favor ingresa una URL válida.');
+    
+  }
+});
